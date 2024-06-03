8a9bef4eae50d9e3fbfb383b78f876eafc2925d3
Krupakar Reddy <137398727+Krupakar-Reddy-S@users.noreply.github.com>
2024-06-03 09:54:46 +0530

    added routing and components code

--- src/App.js
+++ src/App.js
@@ -1,30 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import Typing from './components/Typing/Typing';
+import Tile from './components/Tile/Tile';
+import Banner from './components/Banner';
+import Progress from './components/Progress';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
-import NavBar from './components/NavBar';
-import Typing from './components/Typing';
-
+import { useEffect, useState } from 'react';
 function App() {
+  const [percentage, setPercentage] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setPercentage((prev) => (prev < 100 ? prev + 10 : 100));
+    }, 1000)
+
+    return () => clearInterval(interval);
+  })
   return (
+    <Router>
+
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <NavBar />
       </header>
-      <body>
-        <Typing
-          text = {[
-            "Hi this is a typing demo",
-            "I am a typing effect with React and CSS",
-            ""
+      <Typing
+          text={[
+            "Hello, welcome to the typing effect demo!",
+            "Enjoy exploring this cool effect!",
+            "This is an example of typing and deleting text.",
           ]}
           typingSpeed={100}
           deletingSpeed={50}
           duration={1000}
-        />
-      </body>
-    </div>
+      />
+      <Tile
+        header={"Class is going on "}
+        subHeader={"Everyone is making a tile"}
+        img={"https://cdn.dribbble.com/users/3484830/screenshots/16787618/media/b134e73ef667b926c76d8ce3f962dba2.gif"}
+        text={"Approved training partner under the scheme for market led fee-based services implemented by NSDC"}
+        link={"https://www.scaler.com/"}
+        linkText={"scaler"}
+        position={"img-right"}
+        bgColor={"red"}
+      />
+      {/* <Banner /> */}
+      {/* <Progress percentage={percentage}/> */} 
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </div>
+      </Router>
+
   );
 }
 
 export default App;
+
+
+// import 
+// ./NavBar.js
+// ./NavBar.jsx
+// ./NavBar/index.js
